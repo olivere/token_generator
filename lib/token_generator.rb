6f82edd9081b2d19121ef00b18ab291ddbff4ff4
Oliver Eilhard <oliver.eilhard@gmail.com>
2011-04-29 13:34:53 +0200

    Use AS::SecureRandom for random number generator. Change set_token to use exists? Set default token size to 16 (instead of 12).

--- lib/token_generator.rb
+++ lib/token_generator.rb
@@ -1,15 +1,12 @@
 module TokenGenerator
-  def generate_token(size = 12, &validity)
-    constant = "#{self.class.name}#{id}"
-
+  def generate_token(size = 16, &validity)
     begin
-      token = CGI::Session.generate_unique_id(constant).first(size)
+      token = ActiveSupport::SecureRandom.hex(size / 2)
     end while !validity.call(token) if block_given?
-
     token
   end
 
   def set_token
-    self.token = generate_token { |token| self.class.find_by_token(token).nil? }
+    self.token = generate_token { |token| !self.class.exists?(:token => token) }
   end
 end
